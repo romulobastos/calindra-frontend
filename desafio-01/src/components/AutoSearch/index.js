54089497982d966efa3febf7edf543c14c819b81
Rômulo Bastos <romulobastos.design@gmail.com>
2019-08-31 02:22:26 -0300

    Axios buscando dados com hack para cross-origin

--- desafio-01/src/components/AutoSearch/index.js
+++ desafio-01/src/components/AutoSearch/index.js
@@ -1,29 +1,70 @@
-import React from 'react';
-import "./styles.css"
+import React, { Component } from 'react';
+import "./styles.css";
+import api from '../../services/api';
 
-const AutoSearch = () => (
-	<>
-		<form id="auto-search" action="/busca" method="get">
-			<div className="input-group">
-				<input type="text" autocomplete="off" placeholder="Explore nossos universos" />
-				<button className="search-button">Buscar</button>
-			</div>
-		</form>
-	</>
-)
+export default class AutoSearch extends Component {
 
-{/* <ul class="search-result-list">
-	<li>
-		<a data-value="item" href="/camiseta-rabo-corneo-hungaro-f" class="search-result-item">
-			<span class="product-image">
-				<img data-value="itemimage" src="https://static-store.worldticket.com.br/store/images/721/29721.png" alt="">
-			</span>
-			<span class="product-name">
-				<span data-value="itemname">Camiseta Rabo-Córneo Hungaro</span>
-				<span class="itemprice" data-value="itemprice">R$ 59,90</span>
-			</span>
-		</a>
-	</li>
-</ul> */}
+	state = {
+		products: [],
+	}
 
-export default AutoSearch;
+	componentDidMount(){
+		this.loadProducts();
+	}
+
+	loadProducts = async (text = 'can') => {
+		
+		const response = await api.get(`/autocomplete/${text}`,
+		// {
+		// 	headers: {
+		// 		'Access-Control-Allow-Origin': '*',
+		// 	}
+		// 	,
+		// 	proxy: {
+		// 		host: 'https://store.omelete.com.br/',
+		// 		port: 3128
+		// 	}
+		// }
+		);
+		console.log(response.data);
+		// const { docs, ...productInfo } = response.data;
+		
+		this.setState({
+			products: response.data,
+			// productInfo,
+			// page
+		})
+		// console.log(response.data.docs);
+	};
+
+	render() {
+		return (
+			<form id="auto-search" action="/busca" method="get">
+				<div className="input-group">
+					<input type="text" placeholder="Explore nossos universos" />
+					<button className="search-button">Buscar</button>
+
+					<div className="product-list">
+						<h1>{this.products}</h1>
+					</div>
+				
+				</div>
+			</form>
+			
+			// <ul class="search-result-list">
+			// 	<li>
+			// 		<a data-value="item" href="/camiseta-rabo-corneo-hungaro-f" class="search-result-item">
+			// 			<span class="product-image">
+			// 				<img data-value="itemimage" src="https://static-store.worldticket.com.br/store/images/721/29721.png" alt="">
+			// 			</span>
+			// 			<span class="product-name">
+			// 				<span data-value="itemname">Camiseta Rabo-Córneo Hungaro</span>
+			// 				<span class="itemprice" data-value="itemprice">R$ 59,90</span>
+			// 			</span>
+			// 		</a>
+			// 	</li>
+			// </ul>
+		);
+	}
+
+}
